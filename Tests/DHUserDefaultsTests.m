3cf589eeadce7470ecbc13184b4e34c0debcb414
Dan Hassin <danhassin@mac.com>
2012-05-30 14:53:53 -0400

    Add initial tests

--- Tests/DHUserDefaultsTests.m
+++ Tests/DHUserDefaultsTests.m
@@ -17,6 +17,22 @@
 
 @implementation DHUserDefaultsTests
 
+- (void) test_valid_methods
+{
+	STAssertFalse([DHUserDefaults respondsToSelector:@selector(setSomething:)], @"Class shouldn't respond to any selector");
+	STAssertFalse([DHUserDefaults respondsToSelector:@selector(setSomething:)], @"Class shouldn't respond to any selector");
+
+	//Sets
+	STAssertTrue([[DHUserDefaults standardUserDefaults] respondsToSelector:@selector(setSomething:)], @"Should respond to any selector");
+	STAssertNoThrow([DHUserDefaults standardUserDefaults].something = @"hi", @"Should work with .=");
+	STAssertNoThrow([[DHUserDefaults standardUserDefaults] setSomething:@"hi"], @"Should work with setX:");
+
+	//Gets
+	STAssertTrue([[DHUserDefaults standardUserDefaults] respondsToSelector:@selector(something)], @"Should respond to any selector");
+	STAssertNoThrow([DHUserDefaults standardUserDefaults].something, @"Should work with .");
+	STAssertNoThrow([[DHUserDefaults standardUserDefaults] something], @"Should work with x");
+}
+
 - (void)setUp
 {
     [super setUp];
